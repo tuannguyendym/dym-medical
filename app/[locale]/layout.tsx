fa7817ae8638517f8ad0cc5d98403a129e76f387
Tuan Nguyen <ntuan.2502@gmail.com>
2024-02-02 11:55:54 +0700

    fix cors

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,8 @@ import NextIntlProvider from "./NextIntlClientProvider";
 import "./globals.css";
 import { Providers } from "./providers";
 import { notFound } from "next/navigation";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 type RootLayoutProps = {
   children: React.ReactNode;
@@ -12,7 +14,6 @@ export default async function RootLayout({
   children,
   params,
 }: RootLayoutProps) {
-  
   let messages;
   try {
     messages = (await import(`../../messages/${params.locale}.json`)).default;
@@ -29,7 +30,11 @@ export default async function RootLayout({
           timeZone="Asia/Ho_Chi_Minh"
           now={new Date()}
         >
-          <Providers>{children}</Providers>
+          <Providers>
+            {children}
+            <SpeedInsights />
+            <Analytics />
+          </Providers>
         </NextIntlProvider>
       </body>
     </html>
